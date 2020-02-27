97adccd50832afae46e9019c24639ab261eb7c50
Maszkiewicz, Lisa N <lisa.maszkiewicz@jhuapl.edu>
2020-02-27 17:44:55 -0500

    Finished problem set.

--- Cryptology/crypto_utils.py
+++ Cryptology/crypto_utils.py
@@ -4,31 +4,12 @@ alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 eng_alph_probs = [.082, .015, .028, .043, .127, .022, .020, .061, .070, .002, .008, .040, .024, .067, .075, .019, .001, .060, .063, .091, .028, .010, .023, .001, .020, .001]
 
 
-def calc_ys(k, text):
-  y1 = ""
-  y2 = ""
-  y3 = ""
-  y4 = ""
-  y5 = ""
-  y6 = ""
-  y7 = ""
-  for index in range(len(text)):
-    if index % k == 0:
-      y1 += text[index]
-    elif index % k == 1:
-      y2 += text[index]
-    elif index % k == 2:
-      y3 += text[index]
-    elif index % k == 3:
-      y4 += text[index]
-    elif index % k == 4:
-      y5 += text[index]
-    elif index % k == 5:
-      y6 += text[index]
-    elif index % k == 6:
-      y7 += text[index]
+def let_to_num(letter):
+  return alphabet.index(letter)
 
-  return [y1,y2,y3,y4,y5,y6,y7]
+
+def num_to_let(number):
+  return alphabet[number%26]
   
   
 def xor(str1, str2):
@@ -58,7 +39,67 @@ def index_of_coincidence(text):
   return i_of_c
 	
   
+# to break Vignere cipher
+def calc_ys(k, text):
+  y1 = ""
+  y2 = ""
+  y3 = ""
+  y4 = ""
+  y5 = ""
+  y6 = ""
+  y7 = ""
+  for index in range(len(text)):
+    if index % k == 0:
+      y1 += text[index]
+    elif index % k == 1:
+      y2 += text[index]
+    elif index % k == 2:
+      y3 += text[index]
+    elif index % k == 3:
+      y4 += text[index]
+    elif index % k == 4:
+      y5 += text[index]
+    elif index % k == 5:
+      y6 += text[index]
+    elif index % k == 6:
+      y7 += text[index]
 
+  return [y1,y2,y3,y4,y5,y6,y7]
+  
+
+# modified for stream Vignere cipher
+def calc_ys_mod(k, text):
+  y1 = ""
+  y2 = ""
+  y3 = ""
+  y4 = ""
+  y5 = ""
+  y6 = ""
+  y7 = ""
+  for index in range(len(text)):
+    shift = index//k # how many rotations have happened
+    val = num_to_let(let_to_num(text[index])-shift)
+    if index % k == 0:
+      y1 += val
+      # print("here" +text[index])
+      # print(num_to_let(let_to_num(text[index])-shift))
+    elif index % k == 1:
+      y2 += val
+    elif index % k == 2:
+      y3 += val
+    elif index % k == 3:
+      y4 += val
+    elif index % k == 4:
+      y5 += val
+    elif index % k == 5:
+      y6 += val
+    elif index % k == 6:
+      y7 += val
+
+  return [y1,y2,y3,y4,y5,y6,y7]
+  
+
+# merge table for determining keyword in Vignere
 def calc_M(text):
   N = len(text)
   Mg = [0]*len(eng_alph_probs)
